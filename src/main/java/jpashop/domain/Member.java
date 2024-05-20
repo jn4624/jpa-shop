ec7bb7dd1370f01e869252d6cabc7417c11b0ba6
LimJiNa <jn4624@gmail.com>
2024-05-21 00:59:10 +0900

    엔티티 클래스 개발

--- src/main/java/jpashop/domain/Member.java
+++ src/main/java/jpashop/domain/Member.java
@@ -1,10 +1,11 @@
 package jpashop.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -12,6 +13,14 @@ import lombok.Setter;
 public class Member {
     @Id
     @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
-    private String username;
+
+    private String name;
+
+    @Embedded
+    private Address address;
+
+    @OneToMany(mappedBy = "member")
+    private List<Order> orders = new ArrayList<>();
 }
