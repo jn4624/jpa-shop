52a1aebe9764912aa601ef53e5bef101e54d8c39
LimJiNa <jn4624@gmail.com>
2024-05-20 11:17:05 +0900

    Initial commit

---
+++ src/main/java/jpashop/JpaShopWebApplication.java
@@ -0,0 +1,13 @@
+package jpashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaShopWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaShopWebApplication.class, args);
+	}
+
+}
