a5628a790931df480677432faa444f4b303a847e
LimJiNa <jn4624@gmail.com>
2024-05-21 16:10:21 +0900

    회원 도메인 개발 - 서비스

--- src/main/java/jpashop/service/MemberService.java
+++ src/main/java/jpashop/service/MemberService.java
@@ -1,2 +1,47 @@
-package jpashop.service;public class MemberService {
+package jpashop.service;
+
+import jpashop.domain.Member;
+import jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    /**
+     * 회원가입
+     */
+    @Transactional
+    public Long join(Member member) {
+        // 중복 회원 검증
+        validateDuplicateMember(member);
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Member findMember(Long id) {
+        return memberRepository.findOne(id);
+    }
 }
