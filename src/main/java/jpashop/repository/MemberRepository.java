b7c02d7f1534821fd15a7c2180098a7c060e2427
LimJiNa <jn4624@gmail.com>
2024-05-21 17:57:25 +0900

    회원 도메인 개발 - 레포지토리 생성자 주입으로 변경

--- src/main/java/jpashop/repository/MemberRepository.java
+++ src/main/java/jpashop/repository/MemberRepository.java
@@ -1,16 +1,16 @@
 package jpashop.repository;
 
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
 import jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
+@RequiredArgsConstructor
 public class MemberRepository {
-    @PersistenceContext
-    private EntityManager entityManager;
+    private final EntityManager entityManager;
 
     public void save(Member member) {
         entityManager.persist(member);
