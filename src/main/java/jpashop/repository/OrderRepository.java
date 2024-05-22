eb995ce535b764106a6c03569b8d1ad6d46b666b
LimJiNa <jn4624@gmail.com>
2024-05-22 23:55:39 +0900

    주문 도메인 개발 - 주문 검색 기능 개발

--- src/main/java/jpashop/repository/OrderRepository.java
+++ src/main/java/jpashop/repository/OrderRepository.java
@@ -1,9 +1,16 @@
 package jpashop.repository;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.*;
 import jpashop.domain.Order;
+import jpashop.domain.OrderSearch;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
@@ -18,5 +25,79 @@ public class OrderRepository {
         return entityManager.find(Order.class, id);
     }
 
-    // TODO: findAll 메서드는 주문 검색 기능으로 구현 예정
+    /**
+     * JPQL 처리
+     * 단점: JPQL 쿼리를 문자로 생성하기에 번거롭고, 실수로 인한 버그 발생 확률이 높다.
+     */
+    public List<Order> findAllByString(OrderSearch orderSearch) {
+        String jpql = "select o from Order o join o.member m";
+        boolean isFirstCondition = true;
+
+        // 주문 상태 검색
+        if (orderSearch.getOrderStatus() != null) {
+            if (isFirstCondition) {
+                jpql += " where";
+                isFirstCondition = false;
+            } else {
+                jpql += " and";
+            }
+
+            jpql += " o.status = :status";
+        }
+
+        // 회원 이름 검색
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            if (isFirstCondition) {
+                jpql += " where";
+                isFirstCondition = false;
+            } else {
+                jpql += " and";
+            }
+
+            jpql += " m.name like :name";
+        }
+
+        TypedQuery<Order> orderTypedQuery = entityManager.createQuery(jpql, Order.class)
+                .setMaxResults(1000);
+
+        if (orderSearch.getOrderStatus() != null) {
+            orderTypedQuery = orderTypedQuery.setParameter("status", orderSearch.getOrderStatus());
+        }
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            orderTypedQuery = orderTypedQuery.setParameter("name", orderSearch.getMemberName());
+        }
+
+        return orderTypedQuery.getResultList();
+    }
+
+    /**
+     * JPA Criteria 처리
+     * 단점: 복잡하고 유지보수성이 떨어진다.
+     */
+    public List<Order> findAllByCriteria(OrderSearch orderSearch) {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Order> criteriaQuery = criteriaBuilder.createQuery(Order.class);
+        Root<Order> orderRoot = criteriaQuery.from(Order.class);
+        Join<Object, Object> joinMember = orderRoot.join("member", JoinType.INNER); // 회원과 조인
+
+        List<Predicate> predicates = new ArrayList<>();
+
+        // 주문 상태 검색
+        if (orderSearch.getOrderStatus() != null) {
+            Predicate status = criteriaBuilder.equal(orderRoot.get("status"), orderSearch.getOrderStatus());
+            predicates.add(status);
+        }
+
+        // 회원 이름 검색
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            Predicate name = criteriaBuilder.like(joinMember.<String>get("name"), "%" + orderSearch.getMemberName() + "%");
+            predicates.add(name);
+        }
+
+        criteriaQuery.where(predicates.toArray(new Predicate[predicates.size()]));
+        TypedQuery<Order> orderTypedQuery = entityManager.createQuery(criteriaQuery)
+                .setMaxResults(1000);
+
+        return orderTypedQuery.getResultList();
+    }
 }
