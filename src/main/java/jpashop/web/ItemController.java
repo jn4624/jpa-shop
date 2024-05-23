661d46203ef50cb4e495e6228bc6179ce1b92ac2
LimJiNa <jn4624@gmail.com>
2024-05-23 17:37:25 +0900

    웹 계층 개발 - 상품 등록

---
+++ src/main/java/jpashop/web/ItemController.java
@@ -0,0 +1,38 @@
+package jpashop.web;
+
+import jpashop.domain.item.Book;
+import jpashop.service.ItemService;
+import jpashop.web.form.BookForm;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/items")
+public class ItemController {
+    private final ItemService itemService;
+
+    @GetMapping("/new")
+    public String createForm(Model model) {
+        model.addAttribute("itemForm", new BookForm());
+        return "/items/createItemForm";
+    }
+
+    @PostMapping("/new")
+    public String create(BookForm itemForm) {
+        Book book = new Book();
+        book.setName(itemForm.getName());
+        book.setPrice(itemForm.getPrice());
+        book.setStockQuantity(itemForm.getStockQuantity());
+        book.setAuthor(itemForm.getAuthor());
+        book.setIsbn(itemForm.getIsbn());
+
+        itemService.saveItem(book);
+
+        return "redirect:/";
+    }
+}
