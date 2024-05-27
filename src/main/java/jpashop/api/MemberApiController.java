814289ade4efc50ff0c626538baa056d49f235f4
LimJiNa <jn4624@gmail.com>
2024-05-27 13:59:58 +0900

    API 개발 - 회원 조회 V1

--- src/main/java/jpashop/api/MemberApiController.java
+++ src/main/java/jpashop/api/MemberApiController.java
@@ -9,6 +9,8 @@ import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 public class MemberApiController {
@@ -75,5 +77,26 @@ public class MemberApiController {
     static class UpdateMemberResponse {
         private Long id;
         private String name;
+    }
+
+    /**
+     * 조회 V1: 응답 값으로 엔티티를 직접 외부에 노출한다.
+     *
+     * 문제점:
+     * - 엔티티에 프레젠테이션 계층을 위한 로직이 추가된다.
+     * - 기본적으로 엔티티의 모든 값이 노출된다.
+     * - 응답 스펙을 맞추기 위해 로직이 추가된다. (@JsonIgnore, 별도의 뷰 로직 등)
+     * - 실무에서는 같은 엔티티에 대해 API 가 용도에 따라 다양하게 만들어지는데,
+     *   한 엔티티에 각각의 API 를 위한 프레젠테이션 응답 로직을 담기는 어렵다.
+     * - 엔티티가 변경되면 API 스펙이 변한다.
+     * - 추가로 컬렉션을 직접 반환하면,
+     *   향후 API 스펙을 변경하기 어렵다. (별도의 Result 클래스 생성으로 해결)
+     *
+     * 결론:
+     * - API 응답 스펙에 맞추어 별도의 DTO 를 반환한다.
+     */
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1() {
+        return memberService.findMembers();
     }
 }
