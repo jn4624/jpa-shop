91d080e1e2608b1b1d9090089ba7867b76175500
LimJiNa <jn4624@gmail.com>
2024-05-27 22:59:41 +0900

    API 개발 고급(지연 로딩과 조회 성능 최적화) - 간단한 주문 조회 V1 엔티티 직접 노출(양방향 매핑 설정 무한 루프 해결책 @JsonIgnore)

---
+++ src/main/java/jpashop/api/OrderSimpleApiController.java
@@ -0,0 +1,33 @@
+package jpashop.api;
+
+import jpashop.domain.Order;
+import jpashop.domain.OrderSearch;
+import jpashop.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * xToOne(@ManyToOne, @OneToOne) 관계 최적화
+ * Order
+ * Order -> Member
+ * Order -> Delivery
+ */
+@RestController
+@RequiredArgsConstructor
+public class OrderSimpleApiController {
+    private final OrderRepository orderRepository;
+
+    /**
+     * V1. 엔티티 직접 노출
+     * - Hibernate5JakartaModule(스프링 부트 3.0 이상) 모듈 등록, LAZY = null 처리
+     * - 양방향 관계 문제 발생 -> @JsonIgnore
+     */
+    @GetMapping("/api/v1/simple-orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        return all;
+    }
+}
