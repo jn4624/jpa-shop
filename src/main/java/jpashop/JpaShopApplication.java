53e49a68ed7e86f38dde0c536dbb486c5f79b5b2
LimJiNa <jn4624@gmail.com>
2024-05-27 16:18:16 +0900

    프로젝트명 변경

--- src/main/java/jpashop/JpaShopApplication.java
+++ src/main/java/jpashop/JpaShopApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JpaShopWebApplication {
+public class JpaShopApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JpaShopWebApplication.class, args);
+		SpringApplication.run(JpaShopApplication.class, args);
 	}
 
 }
